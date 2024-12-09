31101e38d59ef0d7f108d4adde87fd96a1595e9f
Lasitha Ransara <114856830+LasithaR01@users.noreply.github.com>
2024-12-09 23:00:17 +0530

    Create Product Section

--- src/main/java/pharmacy/pharmacy/service/ProductService.java
+++ src/main/java/pharmacy/pharmacy/service/ProductService.java
@@ -1,34 +1,39 @@
 package pharmacy.pharmacy.service;
 
 import org.springframework.stereotype.Service;
+import pharmacy.pharmacy.entity.Product;
+import pharmacy.pharmacy.repository.ProductRepository;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ProductService {
 
-//    private final ProductRepository productRepository;
-//
-//    // Constructor injection
-//    public ProductService(ProductRepository productRepository) {
-//        this.productRepository = productRepository;
-//    }
-//
-//    // Fetch all products
-//    public List<Product> getAllProducts() {
-//        return productRepository.findAll();
-//    }
-//
-//    // Fetch a product by ID
-//    public Optional<Product> getProductById(Long id) {
-//        return productRepository.findById(id);
-//    }
-//
-//    // Save or update a product
-//    public Product saveProduct(Product product) {
-//        return productRepository.save(product);
-//    }
-//
-//    // Delete a product by ID
-//    public void deleteProductById(Long id) {
-//        productRepository.deleteById(id);
-//    }
+    private final ProductRepository productRepository;
+
+    // Constructor injection
+    public ProductService(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    // Fetch all products
+    public List<Product> getAllProducts() {
+        return productRepository.findAll();
+    }
+
+    // Fetch a product by ID
+    public Optional<Product> getProductById(Long id) {
+        return productRepository.findById(id);
+    }
+
+    // Save or update a product
+    public Product saveProduct(Product product) {
+        return productRepository.save(product);
+    }
+
+    // Delete a product by ID
+    public void deleteProductById(Long id) {
+        productRepository.deleteById(id);
+    }
 }
