19bb36591edc7d1d081f9959af83988991aeea91
Lasitha Ransara <114856830+LasithaR01@users.noreply.github.com>
2024-12-27 18:29:44 +0530

    Created Category Files
    Controller ,Repo, Service

--- src/main/java/pharmacy/pharmacy/dao/CategoryRepository.java
+++ src/main/java/pharmacy/pharmacy/dao/CategoryRepository.java
@@ -2,21 +2,13 @@ package pharmacy.pharmacy.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import pharmacy.pharmacy.entity.Category;
-<<<<<<< HEAD
-
-public interface CategoryRepository extends JpaRepository<Category, Long> {
-=======
-import pharmacy.pharmacy.entity.Product;
 
 import java.util.Optional;
 import java.util.UUID;
 
 public interface CategoryRepository extends JpaRepository<Category, UUID> {
-    // Check if a category with a specific slug exists
+
     boolean existsBySlug(String slug);
 
-    // Find a product by its slug
     Optional<Category> findBySlug(String slug);
-
->>>>>>> main
 }
