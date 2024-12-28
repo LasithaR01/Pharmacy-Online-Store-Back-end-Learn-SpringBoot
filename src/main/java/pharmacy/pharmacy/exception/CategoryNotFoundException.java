e17b96e3e697d8d2ce01f35593d7cc354b84f9db
Lasitha Ransara <114856830+LasithaR01@users.noreply.github.com>
2024-12-28 22:34:42 +0530

    Create CategoryNotFoundException.java

---
+++ src/main/java/pharmacy/pharmacy/exception/CategoryNotFoundException.java
@@ -0,0 +1,9 @@
+package pharmacy.pharmacy.exception;
+
+public class CategoryNotFoundException extends RuntimeException {
+    public CategoryNotFoundException(String message) {
+
+        super(message);
+    }
+
+}
