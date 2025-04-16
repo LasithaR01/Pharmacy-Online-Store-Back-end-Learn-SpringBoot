2127e51cabf94d0e376da7087abf23dc3a07f296
Lasitha Ransara <114856830+LasithaR01@users.noreply.github.com>
2025-04-16 22:59:26 +0530

    feat: ROLE_ADMIN, ROLE_USER
    Automatically create 2 roles in the database if not exist table roles.
    names -> ROLE_ADMIN, ROLE_USER

---
+++ src/main/java/pharmacy/pharmacy/dao/RoleRepository.java
@@ -0,0 +1,14 @@
+package pharmacy.pharmacy.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import pharmacy.pharmacy.entity.ERole;
+import pharmacy.pharmacy.entity.Role;
+
+import java.util.Locale;
+import java.util.Optional;
+
+public interface RoleRepository extends JpaRepository<Role, Long> {
+    Optional<Role> findByName(ERole name);
+
+    boolean existsByName(ERole eRole);
+}
