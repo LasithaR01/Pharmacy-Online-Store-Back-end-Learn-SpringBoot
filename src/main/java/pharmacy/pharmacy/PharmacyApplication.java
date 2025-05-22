cdeee3e5d01eaa55bbe0cee9f266aa8d7ea9d292
Lasitha Ransara <114856830+LasithaR01@users.noreply.github.com>
2025-05-22 22:31:49 +0530

    feat: Integrated Sentry

--- src/main/java/pharmacy/pharmacy/PharmacyApplication.java
+++ src/main/java/pharmacy/pharmacy/PharmacyApplication.java
@@ -1,5 +1,6 @@
 package pharmacy.pharmacy;
 
+import io.sentry.Sentry;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
@@ -10,7 +11,14 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 public class PharmacyApplication {
 
     public static void main(String[] args) {
+        // Initialize Sentry
+        Sentry.init(options -> {
+            options.setDsn("https://b756c10294ad1a6d1f8c3d8a09c805e8@o4509248313032704.ingest.us.sentry.io/4509366128279552");
+            options.setEnvironment("production");
+            options.setRelease("sethma-app@1.0.0");
+            // You can add more configuration here if needed
+        });
+
         SpringApplication.run(PharmacyApplication.class, args);
     }
-
 }
