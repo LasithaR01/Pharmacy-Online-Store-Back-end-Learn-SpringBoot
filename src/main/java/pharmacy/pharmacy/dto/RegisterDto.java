d5312cd329d2459bd88fc9e4ff92eb12c5686844
Lasitha Ransara <114856830+LasithaR01@users.noreply.github.com>
2025-06-10 20:33:30 +0530

    Update RegisterDto.java

--- src/main/java/pharmacy/pharmacy/dto/RegisterDto.java
+++ src/main/java/pharmacy/pharmacy/dto/RegisterDto.java
@@ -5,7 +5,7 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import java.util.Set;
 
-public class RegisterDto {
+public class RegisterDTO {
     @NotBlank(message = "Username cannot be blank")
     @Size(min = 3, max = 50, message = "Username must be between 3 and 50 characters")
     private String username;
